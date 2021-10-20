865d742099f71cb70210b7ce9157396a4562ba33
alpha-tango-kilo <git@heyatk.com>
2021-10-20 15:23:14 +0100

    Silly program to check size of Game in memory
    As of this commit:
    Size of Game<3>: 240 bytes
    Size of Game<4>: 296 bytes
    Size of Game<5>: 352 bytes
    Size of Game<6>: 416 bytes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::mem;
+use game::Game;
+
 fn main() {
-    println!("Hello, world!");
+    println!(
+        "Size of Game<3>: {} bytes",
+        mem::size_of::<Game<3>>(),
+    );
+    println!(
+        "Size of Game<4>: {} bytes",
+        mem::size_of::<Game<4>>(),
+    );
+    println!(
+        "Size of Game<5>: {} bytes",
+        mem::size_of::<Game<5>>(),
+    );
+    println!(
+        "Size of Game<6>: {} bytes",
+        mem::size_of::<Game<6>>(),
+    );
 }
