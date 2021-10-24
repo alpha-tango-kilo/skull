9c3918c8b299cea39b8521145a5fd40a7a7b45ab
alpha-tango-kilo <git@heyatk.com>
2021-10-24 22:16:39 +0100

    Document the rest of lib.rs
    Event, InputType, Response, ResponseError
    Fix some missing fixed-width formatting

--- game/src/lib.rs
+++ game/src/lib.rs
@@ -25,7 +25,7 @@
 //! # What does this crate provide?
 //!
 //! This crate provides a **simulation** of the game Skull.
-//! It allows for the creation of a [Game] for 3 to 6 players (as recommended
+//! It allows for the creation of a [`Game`] for 3 to 6 players (as recommended
 //! by the original) and provides all necessary means to interact with the
 //! game and understand the current state of the game.
 //! It enforces all of the games rules and scoring for you, so you only need
@@ -104,7 +104,7 @@ impl fmt::Display for Card {
 /// State is generic over the number of players
 ///
 /// It is expected that you would only ever get a State by calling
-/// [Game::state()], instead of creating one
+/// [`Game::state()`], instead of creating one
 ///
 #[derive(Debug, Clone, Eq, PartialEq)]
 pub enum State<const N: usize> {
@@ -140,49 +140,103 @@ pub enum State<const N: usize> {
     },
 }
 
+/// An event is either a notification of something important, or a prompt to
+/// tell the user/programmer what input is expected
+///
+/// You can think of them a bit like a notification (except `Input`)
+///
+/// You are **required** to process all pending events before giving the game
+/// another input.
+/// See [`Game::what_next()`] for more information
+///
 #[derive(Debug, Copy, Clone, Eq, PartialEq)]
 pub enum Event {
+    /// Indicates that an input is required from one of the game's players
     Input {
+        /// Which player (by index) should be making the input
         player: usize,
+        /// What sort of input the game is expecting
         input: InputType,
     },
+    /// Notifies that a bid has started and the game [`State`] has changed
     BidStarted,
+    /// Notifies that a challenge has started and the game [`State`] has
+    /// changed
     ChallengeStarted,
+    /// Notifies that a challenge has ended because the challenger flipped a
+    /// skull.
+    /// The game [`State`] has changed
     ChallengerChoseSkull {
+        /// The index of the challenger
         challenger: usize,
+        /// The index of the skull player
+        /// (can potentially be the same as `challenger`)
         skull_player: usize,
     },
+    /// Notifies that a player has lost all their cards
+    /// (index of player provided)
     PlayerOut(usize),
+    /// Notifies that the challenger won their challenge
+    /// (index of challenger provided)
     ChallengeWon(usize),
+    /// Notifies that the challenger won their challenge and has now won the
+    /// game (index of winner provided)
     ChallengeWonGameWon(usize),
 }
 
+/// The type of input required from the player
 #[derive(Debug, Copy, Clone, Eq, PartialEq)]
 pub enum InputType {
+    /// The player must play a card
     PlayCard,           // When not everyone has played a card
+    /// The player can either play a card or start a bid
     PlayCardOrStartBid, // When everyone has played a card
+    /// The player must start a bid
     StartBid,           // When player has no cards remaining
+    /// The player must bid or pass
     BidOrPass,
+    /// The challenger must flip a card
     FlipCard,
 }
 
+/// The type of input given to the game using [`Game::respond()`]
 #[derive(Debug, Copy, Clone, Eq, PartialEq)]
 pub enum Response {
+    /// The current player plays the specified card
     PlayCard(Card),
+    /// The current bidder raises the bid to the specified number
     Bid(usize),
+    /// The current bidder opts to pass
     Pass,
+    /// The challenger flips over a card `(player_index, card_index)`
     Flip(usize, usize),
 }
 
+/// The type of error produced by [`Game::respond()`]
 #[derive(Debug, Copy, Clone, Eq, PartialEq)]
 pub enum ResponseError {
+    /// Can't take an input now because there is another [`Event`] that needs
+    /// processing.
+    /// Call [`Game::what_next()`]
     PendingEvent,
+    /// Input type didn't match what was expected.
+    /// Correct [`InputType`] provided
     IncorrectInputType(InputType),
+    /// Player tried to put down a card they don't have
+    /// (either already played, or discarded)
     CardNotInHand,
+    /// Bid submitted was lower than current bid.
+    /// Minimum acceptable bid provided
     BidTooLow(usize),
+    /// Bid submitted was too high (in excess of the number of cards played).
+    /// Maximum acceptable bid provided
     BidTooHigh(usize),
+    /// Out of range index given when challenger tried to flip a card
     InvalidIndex,
+    /// Challenger is trying to flip a card they've already flipped
     CardAlreadyFlipped,
+    /// Challenger is trying to flip their own cards
+    /// (they're flipped automatically)
     ManuallyFlippingOwnCards,
 }
 
